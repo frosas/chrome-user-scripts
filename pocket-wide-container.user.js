06130ce8b78a75e35c397742f83adb444bec68a4
Francesc Rosàs <francescrosasbosque@gmail.com>
2013-01-07 19:03:15 +0100

    Pocket Wide Container

---
+++ pocket-wide-container.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name Pocket Wide Container
+// @match https://getpocket.com/a/queue/
+// @version 1.1
+// ==/UserScript==
+ 
+var style = document.createElement('style')
+style.innerHTML = [
+    '.wrapper { width: auto }',
+    '.item { width: 18.3% !important }',
+    '#page #content .wrapper.tag_sidebar_holder { background-image: none }'
+].join('\n')
+document.head.appendChild(style)
