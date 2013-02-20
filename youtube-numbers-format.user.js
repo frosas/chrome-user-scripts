bd9ec1c1f96228c3008ab43b58c0ba3930edc1b2
Francesc Rosàs <francescrosasbosque@gmail.com>
2013-02-20 09:06:32 +0100

    Refacts

--- youtube-numbers-format.user.js
+++ youtube-numbers-format.user.js
@@ -1,26 +1,20 @@
 // ==UserScript==
 // @name YouTube Numbers Format
-// @version 0.2
+// @version 0.2.1
 // @match http://www.youtube.com/watch?*
 // ==/UserScript==
  
 var formatNumbers = function(string) {
-    return string.replace(/[0-9]+/, function(matched) {
-        return formatNumber(matched)
-    })
+    return string.replace(/\d+/, function(matched) { return formatNumber(matched) })
 }
 
 var formatNumber = function(number) {
-    return number.split('').reverse().groupByLength(3).reverse().join('.')
-}
-
-Array.prototype.groupByLength = function(length) {
-    var grouped = []
-    this.forEach(function(value, index) {
-        if (index % length === 0) grouped.push('')
-        grouped[grouped.length - 1] = value + grouped[grouped.length - 1]
-    })
-    return grouped
+    var formatted = ''
+    for (var i = 0; i < number.length; i++) {
+        if (i && i % 3 == 0) formatted = '.' + formatted
+        formatted = number[number.length - 1 - i] + formatted
+ }
+    return formatted
 }
 
 ;['.watch-view-count', '.likes-count', '.dislikes-count', '.view-count'].forEach(function(selector) {
