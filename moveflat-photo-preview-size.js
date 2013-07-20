e5387fb2ddb4f2faca172f4e28c29541f5f07f5d
Francesc Rosàs <francescrosasbosque@gmail.com>
2013-07-21 00:19:48 +0100

    Create moveflat-photo-preview-size.js

---
+++ moveflat-photo-preview-size.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name Moveflat - Photo max size
+// @version 0.1
+// @match http://www.moveflat.com/*
+// ==/UserScript==
+
+document.querySelector('#pic').style.maxHeight = '300px'
