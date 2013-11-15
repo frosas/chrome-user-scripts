00989c6442d924cfdb54ce3a781045a23c1dcd77
Francesc Rosàs <francescrosasbosque@gmail.com>
2013-11-15 13:48:18 +0000

    Delete moveflat-photo-preview-size.js

--- moveflat-photo-preview-size.js
+++
@@ -1,7 +0,0 @@
-// ==UserScript==
-// @name Moveflat - Photo max size
-// @version 0.1
-// @match http://www.moveflat.com/*
-// ==/UserScript==
-
-document.querySelector('#pic').style.maxHeight = '300px'
