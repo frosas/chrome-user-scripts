7c6c21e711c768c1615e31926a3fc13053eb5ed3
Francesc Rosàs <francescrosasbosque@gmail.com>
2013-12-08 21:00:33 +0000

    Make it work directly as a Chrome user script

--- google-calendar-larger-tasks-textarea.user.js
+++ google-calendar-larger-tasks-textarea.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Google Calendar Larger Tasks Textarea
-// @version 0.1
-// @match http*://www.google.com/calendar/render
+// @version 0.1.1
+// @match *://www.google.com/calendar/render*
 // ==/UserScript==
  
 setInterval(function() {
