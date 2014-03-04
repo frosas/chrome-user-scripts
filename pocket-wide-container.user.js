0cc206fbb79be1413b69c3f1158442ede6997928
Francesc Rosas <francescrosasbosque@gmail.com>
2014-03-04 01:56:18 +0000

    Always open the external link

--- pocket-wide-container.user.js
+++ pocket-wide-container.user.js
@@ -11,3 +11,15 @@ style.innerHTML = [
     '#page #content .wrapper.tag_sidebar_holder { background-image: none }'
 ].join('\n')
 document.head.appendChild(style)
+
+// Always open the external link
+new MutationObserver(function(mutations) { 
+    mutations.forEach(function(mutation) { 
+        ;[].forEach.call(mutation.addedNodes || [], function(node) {
+            ;[].forEach.call(node.parentNode.querySelectorAll('.original_url'), function(a) {
+                var item = (function(node) { while (!node.classList.contains('item')) node = node.parentNode; return node })(a)
+                item.querySelector('.link').href = a.href
+            })
+        }) 
+    }) 
+}).observe(document, {childList: true, subtree: true})
